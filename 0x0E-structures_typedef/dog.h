561370072ce1a51ece8307c9c2137a3d999ed87f
Rwikwiza <arwikwiza@gmail.com>
2023-04-18 00:54:04 +0200

    dog structure declaration

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,25 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
-* struct dog - struct that stores some information of a dog.
-* @name: name of the dog.
-* @age: age of the dog.
-* @owner: owner of the dog.
-*/
-struct dog {
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+/**
+ * struct dog - struct that stores some information of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
 
-#endif /* DOG_H */
+#endif
