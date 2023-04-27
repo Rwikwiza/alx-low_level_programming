48fa5f896aa86214696f0fb31b2513f065ea8cfb
Rwikwiza <arwikwiza@gmail.com>
2023-04-27 05:23:59 -0700

    removed spaces

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,8 +16,8 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", h->len, h->str);
-		h = h-> next;
-		elnum ++;
+		h = h->next;
+		elnum++;
 	}
 	return (elnum);
 }
