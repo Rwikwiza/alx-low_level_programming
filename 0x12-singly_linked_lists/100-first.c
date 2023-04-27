71b0be946083f5287036e70d77e88f004496cd70
Rwikwiza <arwikwiza@gmail.com>
2023-04-27 23:58:11 +0200

    prints before main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void printIntro() {
-    printf("You're beat! and yet, you must allow,\n");
-    printf("I bore my house upon my back!\n");
-}
+/**
+ * bmain - function executed before main
+ * Return: no return.
+ */
 
-int main() {
-    printIntro();
-    // rest of the code
-    return 0;
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
 }
