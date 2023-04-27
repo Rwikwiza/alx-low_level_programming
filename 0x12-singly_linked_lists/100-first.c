7e03287ede3f0c9895bc1e5e80d347c9c57d1082
Rwikwiza <arwikwiza@gmail.com>
2023-04-27 23:56:54 +0200

    prints before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void printIntro() {
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
+}
+
+int main() {
+    printIntro();
+    // rest of the code
+    return 0;
+}
