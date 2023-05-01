af50daa60ea83dd4e260bcd783d21aa744ebc715
Rwikwiza <arwikwiza@gmail.com>
2023-05-01 22:12:44 +0200

    returns number of elements

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/*listin_len  - returns the number of elements in
+ * a linked list.
+ * @h: head of a list.
+ *
+ * Return: number of nodes.
+ */
+size_t listin_len(const listnt_t *h)
+{
+	size_t nnodes = 0;
+
+	while (h!= NULL)
+	{
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
+}
