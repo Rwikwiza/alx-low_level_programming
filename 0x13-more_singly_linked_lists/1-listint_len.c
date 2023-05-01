26dcf52f336d7f13d039c0aae2a5a196e82aab62
Rwikwiza <arwikwiza@gmail.com>
2023-05-01 22:47:53 +0200

    typo fixing

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
-/*listin_len  - returns the number of elements in
+/*listint_len  - returns the number of elements in
  * a linked list.
  * @h: head of a list.
  *
  * Return: number of nodes.
  */
-size_t listint_len(const listnt_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t nnodes = 0;
 
