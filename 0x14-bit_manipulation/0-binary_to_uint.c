4b8f2e43eabc49f108dec968daa22b186f5653d3
Rwikwiza <arwikwiza@gmail.com>
2023-05-05 08:12:39 +0200

    converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -17,12 +17,12 @@ unsigned int binary_to_uint(const char *b)
 
 	ui = 0;
 
-	for (len = 0; b(len) != '\0'; len++)
+	for (len = 0; b[len] != '\0'; len++)
 		;
 
 	for (len --, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[len] != '0' && b(len) != '1')
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
