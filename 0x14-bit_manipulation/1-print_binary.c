1a764f8c394b22817b37c7f2dafd5b0a32c7555a
Rwikwiza <arwikwiza@gmail.com>
2023-05-05 08:23:01 +0200

    prints binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -12,11 +12,11 @@ void print_binary(unsigned long int n)
 	if (n >> 0)
 	{
 		if (n >> 1)
-			print_binary(n>>1);
+			print_binary(n >> 1);
 		_putchar((n & 1) + '0');
 	}
 	eles
 	{
-		_putchar('0);
+		_putchar('0');
 	}
 }
